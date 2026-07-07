dc024a6073137aea812a57ad0df6c567618903df
GGomesServicos <guilherme@ggomes.dev.br>
2026-07-07 10:56:21 +0000

    modificando a página do site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home( ){
-    return <h1>Meu teste editando essa página para aprender de verdade sobre GitHub</h1>
+    return <h1>Agora testando deploy com Vercel</h1>
 }
 
 export default Home;
